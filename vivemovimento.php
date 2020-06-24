fcfb310023aacd564d9d72267a5212d690517255
svc93 <svc9304@gmail.com>
2020-06-23 22:06:37 -0600

    Se oculta opcion de registro
    Solicito en la tarde de hoy por audio en whastapp.

--- vivemovimento.php
+++ vivemovimento.php
@@ -257,7 +257,7 @@ function fnViveMovimento_topbar(){
                 <a href="/wp-login.php/" class="a_top" style="color: white;"><i class="fa fa-sign-in" aria-hidden="true"></i> Iniciar Sesión
                 </a>
             </li>
-            <li>
+            <li style="display:none;">
                 <a href="/wp-register.php" class="a_top" style="color: white;"><i class="fa fa-sign-in" aria-hidden="true"></i> Registrarse
                 </a>
             </li>
