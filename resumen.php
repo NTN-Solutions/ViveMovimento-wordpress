50564f82e2fe471217177f3ccfa3b55a2202b6b1
svc93 <svc9304@gmail.com>
2020-06-30 22:21:07 -0600

    Mejora 6 - Fin 10:21pm
    6. Un espacio en el dashboard para poner en cada paso un video informativo jajaja
    R:  Aca podemos poner un botón de video, y cuando el usuario le de click abrimos una ventana flotante y con el video, si tienes los URL de youtube los necesito para anclarlos en cada click que haga, esta mejora tomaría de una a dos horas. Ok voy a hacer estos videos entonces :)

--- resumen.php
+++ resumen.php
@@ -14,8 +14,25 @@
     	$strUsuario = wp_get_current_user()->user_login;
       fnViveMovimento_resumen_data();
         //$datFechaDiario = new DateTime();
-      
+      if (isset($_GET['action']) && $_GET['action'] == 'dashboard' && isset($_POST['intOpcion']) && $_POST['intOpcion'] != null && $_POST['intOpcion'] == '1') {
+        $strURL_paso_1 = $_POST['decId_1'];
+        $strURL_paso_2 = $_POST['decId_2'];
+        $strURL_paso_3 = $_POST['decId_3'];
+        $strURL_paso_4 = $_POST['decId_4'];
+        $strURL_paso_5 = $_POST['decId_5'];
+        $strURL_paso_6 = $_POST['decId_6'];
+        $strURL_paso_7 = $_POST['decId_7'];
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_1' WHERE D.decId = 1");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_2' WHERE D.decId = 2");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_3' WHERE D.decId = 3");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_4' WHERE D.decId = 4");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_5' WHERE D.decId = 5");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_6' WHERE D.decId = 6");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_7' WHERE D.decId = 7");
+      }
+
       $bitPermisoAdmin = ( in_array( 'administrator', wp_get_current_user()->roles, true ) );
+      $listPasosVideos = $wpdb->get_results("SELECT * FROM wp_vivemov_pasos_videos ORDER BY decId ASC;");
 
 ?>
 
@@ -26,28 +43,63 @@
     <h4><?php echo iconv('ISO-8859-1', 'UTF-8', strftime('%A, %d/%B/%Y', time())); ?></h4>
 
     <div class="alert alert-<?php echo ($gDataPaso_1 == null ? 'warning' : 'success') ?>" role="alert">
-      <strong><a href="/user/?action=tab_Paso_1"> Paso 1 - </strong> Información Básica (<?php echo ($gDataPaso_1 == null ? 'PENDIENTE' : 'Completada') ?>)</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_1"> Paso 1 - </strong> Información Básica (<?php echo ($gDataPaso_1 == null ? 'PENDIENTE' : 'Completada') ?>)
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[0]->strURL != null && $listPasosVideos[0]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[0]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
 
     <div class="alert alert-<?php echo ($gDataPaso_2 == null ? 'warning' : 'success') ?>" role="alert">
-      <strong><a href="/user/?action=tab_Paso_2"> Paso 2 - </strong> Gasto Energético (<?php echo ($gDataPaso_2 == null ? 'PENDIENTE' : 'Completada') ?>)</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_2"> Paso 2 - </strong> Gasto Energético (<?php echo ($gDataPaso_2 == null ? 'PENDIENTE' : 'Completada') ?>)
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[1]->strURL != null && $listPasosVideos[1]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[1]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
 
     <div class="alert alert-<?php echo ($gDataPaso_3 == null ? 'warning' : 'success') ?>" role="alert">
-      <strong><a href="/user/?action=tab_Paso_3"> Paso 3 - </strong> Metas (<?php echo ($gDataPaso_3 == null ? 'PENDIENTE' : 'Completada') ?>)</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_3"> Paso 3 - </strong> Metas (<?php echo ($gDataPaso_3 == null ? 'PENDIENTE' : 'Completada') ?>)
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[2]->strURL != null && $listPasosVideos[2]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[2]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
 
     <div class="alert alert-info" role="alert">
-      <strong><a href="/user/?action=tab_Paso_4"> Paso 4 - </strong> Conoce tu Plan Nutricional</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_4"> Paso 4 - </strong> Conoce tu Plan Nutricional
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[3]->strURL != null && $listPasosVideos[3]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[3]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
     <div class="alert alert-info" role="alert">
-      <strong><a href="/user/?action=tab_Paso_8"> Paso 5 - </strong> Elige tu Plan de Ejercicios</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_8"> Paso 5 - </strong> Elige tu Plan de Ejercicios
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[4]->strURL != null && $listPasosVideos[4]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[4]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
     <div class="alert alert-info" role="alert">
-      <strong><a href="/user/?action=tab_Paso_5"> Paso 6 - </strong> Ingresa tus comidas en tu Food Journal</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_5"> Paso 6 - </strong> Ingresa tus comidas en tu Food Journal
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[5]->strURL != null && $listPasosVideos[5]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[5]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
     <div class="alert alert-info" role="alert">
-      <strong><a href="/user/?action=tab_Paso_7"> Paso 7 - </strong> Lleva control de tu progreso</a>
+      <strong>
+        <a href="/user/?action=tab_Paso_7"> Paso 7 - </strong> Lleva control de tu progreso
+        </a>
+        <?php if ($listPasosVideos != null && $listPasosVideos[6]->strURL != null && $listPasosVideos[6]->strURL != '') {
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[6]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+        } ?>
     </div>
   </div>
 </div>
@@ -63,25 +115,63 @@
 
 
 
-  if ($bitPermisoAdmin == true) { ?>
+  if ($bitPermisoAdmin == true) {
 
-<div class="table-responsive">
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <th>Paso</th>
-          <th>URL Video</th>
-        </tr>
-      </thead>
+?>
+<form action="<?php echo strtok($_SERVER["REQUEST_URI"],'?');?>?action=dashboard" method="post" class="row">
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+          <tr>
+            <th>Paso</th>
+            <th>URL Video</th>
+          </tr>
+        </thead>
       <tbody>
-    <tr class="info">
-      <td>1</td>
-      <td>URL</td>
-    </tr>
-  </table>
+          <input type="hidden" name="intOpcion" value="1">
+        <?php
+          $intContadorPasos = 1;
+          foreach ($listPasosVideos as $key) {
+            echo '<tr class="info">
+                    <td>'.$intContadorPasos.'</td>
+                    <td><input type="text" class="form-control" name="decId_'.$intContadorPasos.'" placeholder="Url youtube" value="'.$key->strURL.'"></td>
+                  </tr>';
+            $intContadorPasos += 1;
+          }
+        ?>
+      </tbody>
+    </table>
+  </div>
+<button type="submit" class="btn btn-primary btn-sm btn-block">Guardar</button>
+</form>
+
+<?php  } ?>
+
+
+<div class="modal fade" id="modalPasosVideo" tabindex="-1" role="dialog" aria-labelledby="modalPasosVideoLabel" data-backdrop="false">
+  <div class="modal-dialog" role="document" style="margin-top: 7%;width: 99%">
+    <div class="modal-content">
+      <div class="modal-header" style="padding: 0px;text-align: center;">
+        <h4 class="modal-title" id="modalPasosVideoLabel">Ayuda</h4>
+      </div>
+      <div class="modal-body" style="padding: 0px;text-align: center;">
+        <iframe id="iframePasosVideos" width="100%" height="500" src="about:blank"></iframe>
+      </div>
+      <div class="modal-footer" style="padding: 0px;text-align: center;">
+        <button type="button" class="btn btn-default btn-sm btn-block" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
 </div>
 
+<script>
+  function fnPasosMostrarVideo(strUrl) {
+    $('#iframePasosVideos').attr('src', strUrl);
+    $('#modalPasosVideo').modal('show');
+  }
+</script>
 
-<?php  }
+<?php
+
 }
 ?>
--- vivemovimento.php
+++ vivemovimento.php
@@ -295,7 +295,7 @@ function fnFontawesome() {
             }
           </style>';
 
-    if ( is_page('Perfil') || is_page('User') || is_page('Settings') ): 
+    if ( is_page('Perfil') || is_page('User') || is_page('Settings') || is_page('mi-cuenta') ): 
         echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">';
         echo '<script src="https://kit.fontawesome.com/1ea98a15ec.js" crossorigin="anonymous"></script>';
         // echo '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="crossorigin="anonymous"></script>';
@@ -305,7 +305,7 @@ function fnFontawesome() {
     endif;
 }
 function fnFontawesome2() {
-    if ( is_page('Perfil') || is_page('User') || is_page('Settings') ): 
+    if ( is_page('Perfil') || is_page('User') || is_page('Settings')  || is_page('mi-cuenta') ): 
         echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
         echo '<script src="/wp-content/plugins/vivemovimento/datepicker/js/bootstrap-datepicker.min.js"></script>';
     endif;
