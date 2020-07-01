4378b224ea089a839ca19832f2ff3cbe257a86f1
svc93 <svc9304@gmail.com>
2020-06-30 22:32:19 -0600

    Mejora 7 - Fin 10:32 pm
    7. agregar todos los tabs que faltan del google sheet en el dashboard- preguntas frecuentes, guia de suplementos, guia de habitos, y uno que sea una forma de contacto como para consultas por correo
    R: "Agregar todos" no lo veo correcto, necesitamos especificar porque de lo contrario no tendremos un objetivo establecido; sobre preguntas frecuentes, guia de suplementos y guia de hábitos, esto es texto plano? o es un documento word?. En caso sea texto plano podemos implementar que tu puedas escribir y poder editarlo, pero los clientes solo podrán ver la información guardada por tu usuario admin, esta mejora tomaría de tres a cuatro horas. Texto plano pero lo quiero bonito, podria editar tambien el structure de la pagina o solo texto? Porque por ejemplo en las pages que he agregado puedo poner widgets como slides y cosas por el estilo. O sea yo lo que quiero en el dashboard es que me agregues las tabs con click a link que te redirect a estas paginas, vos me haces eso y yo hago las paginas sin problema :) Es solo linkear las paginas con el dashboard!

--- resumen.php
+++ resumen.php
@@ -22,13 +22,22 @@
         $strURL_paso_5 = $_POST['decId_5'];
         $strURL_paso_6 = $_POST['decId_6'];
         $strURL_paso_7 = $_POST['decId_7'];
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_1' WHERE D.decId = 1");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_2' WHERE D.decId = 2");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_3' WHERE D.decId = 3");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_4' WHERE D.decId = 4");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_5' WHERE D.decId = 5");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_6' WHERE D.decId = 6");
-        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_7' WHERE D.decId = 7");
+
+        $strAyuda_1 = $_POST['strAyuda_1'];
+        $strAyuda_2 = $_POST['strAyuda_2'];
+        $strAyuda_3 = $_POST['strAyuda_3'];
+        $strAyuda_4 = $_POST['strAyuda_4'];
+        $strAyuda_5 = $_POST['strAyuda_5'];
+        $strAyuda_6 = $_POST['strAyuda_6'];
+        $strAyuda_7 = $_POST['strAyuda_7'];
+
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_1', D.strAyudaLink = '$strAyuda_1' WHERE D.decId = 1");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_2', D.strAyudaLink = '$strAyuda_2' WHERE D.decId = 2");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_3', D.strAyudaLink = '$strAyuda_3' WHERE D.decId = 3");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_4', D.strAyudaLink = '$strAyuda_4' WHERE D.decId = 4");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_5', D.strAyudaLink = '$strAyuda_5' WHERE D.decId = 5");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_6', D.strAyudaLink = '$strAyuda_6' WHERE D.decId = 6");
+        $wpdb->get_results("UPDATE wp_vivemov_pasos_videos as D SET D.strURL = '$strURL_paso_7', D.strAyudaLink = '$strAyuda_7' WHERE D.decId = 7");
       }
 
       $bitPermisoAdmin = ( in_array( 'administrator', wp_get_current_user()->roles, true ) );
@@ -47,7 +56,10 @@
         <a href="/user/?action=tab_Paso_1"> Paso 1 - </strong> Información Básica (<?php echo ($gDataPaso_1 == null ? 'PENDIENTE' : 'Completada') ?>)
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[0]->strURL != null && $listPasosVideos[0]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[0]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[0]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[0]->strAyudaLink != null && $listPasosVideos[0]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[0]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
 
@@ -56,7 +68,10 @@
         <a href="/user/?action=tab_Paso_2"> Paso 2 - </strong> Gasto Energético (<?php echo ($gDataPaso_2 == null ? 'PENDIENTE' : 'Completada') ?>)
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[1]->strURL != null && $listPasosVideos[1]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[1]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[1]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[1]->strAyudaLink != null && $listPasosVideos[1]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[1]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
 
@@ -65,7 +80,10 @@
         <a href="/user/?action=tab_Paso_3"> Paso 3 - </strong> Metas (<?php echo ($gDataPaso_3 == null ? 'PENDIENTE' : 'Completada') ?>)
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[2]->strURL != null && $listPasosVideos[2]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[2]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[2]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[2]->strAyudaLink != null && $listPasosVideos[2]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[2]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
 
@@ -74,7 +92,10 @@
         <a href="/user/?action=tab_Paso_4"> Paso 4 - </strong> Conoce tu Plan Nutricional
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[3]->strURL != null && $listPasosVideos[3]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[3]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[3]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[3]->strAyudaLink != null && $listPasosVideos[3]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[3]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
     <div class="alert alert-info" role="alert">
@@ -82,7 +103,10 @@
         <a href="/user/?action=tab_Paso_8"> Paso 5 - </strong> Elige tu Plan de Ejercicios
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[4]->strURL != null && $listPasosVideos[4]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[4]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[4]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[4]->strAyudaLink != null && $listPasosVideos[4]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[4]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
     <div class="alert alert-info" role="alert">
@@ -90,7 +114,10 @@
         <a href="/user/?action=tab_Paso_5"> Paso 6 - </strong> Ingresa tus comidas en tu Food Journal
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[5]->strURL != null && $listPasosVideos[5]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[5]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[5]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[5]->strAyudaLink != null && $listPasosVideos[5]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[5]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
     <div class="alert alert-info" role="alert">
@@ -98,7 +125,10 @@
         <a href="/user/?action=tab_Paso_7"> Paso 7 - </strong> Lleva control de tu progreso
         </a>
         <?php if ($listPasosVideos != null && $listPasosVideos[6]->strURL != null && $listPasosVideos[6]->strURL != '') {
-          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[6]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i> Ayuda</span>';
+          echo '<span style="float: right;cursor: pointer;" onClick="fnPasosMostrarVideo('."'".$listPasosVideos[6]->strURL."'".');"><i class="fa fa-video-camera" aria-hidden="true"></i></span>';
+        } ?>
+        <?php if ($listPasosVideos != null && $listPasosVideos[6]->strAyudaLink != null && $listPasosVideos[6]->strAyudaLink != '') {
+          echo '<a style="float: right;cursor: pointer;" href="'.$listPasosVideos[6]->strAyudaLink.'"><i class="fa fa-question-circle" aria-hidden="true"></i></a>';
         } ?>
     </div>
   </div>
@@ -118,32 +148,38 @@
   if ($bitPermisoAdmin == true) {
 
 ?>
-<form action="<?php echo strtok($_SERVER["REQUEST_URI"],'?');?>?action=dashboard" method="post" class="row">
-  <div class="table-responsive">
-    <table class="table table-striped">
-      <thead>
-          <tr>
-            <th>Paso</th>
-            <th>URL Video</th>
-          </tr>
-        </thead>
-      <tbody>
-          <input type="hidden" name="intOpcion" value="1">
-        <?php
-          $intContadorPasos = 1;
-          foreach ($listPasosVideos as $key) {
-            echo '<tr class="info">
-                    <td>'.$intContadorPasos.'</td>
-                    <td><input type="text" class="form-control" name="decId_'.$intContadorPasos.'" placeholder="Url youtube" value="'.$key->strURL.'"></td>
-                  </tr>';
-            $intContadorPasos += 1;
-          }
-        ?>
-      </tbody>
-    </table>
+<div class="row">
+  <div class="col-md-12 col-xs-12 col-sm-12">
+    <form action="<?php echo strtok($_SERVER["REQUEST_URI"],'?');?>?action=dashboard" method="post" class="row">
+      <div class="table-responsive">
+        <table class="table table-striped">
+          <thead>
+              <tr>
+                <th>Paso</th>
+                <th>URL Video</th>
+                <th>URL Ayuda</th>
+              </tr>
+            </thead>
+          <tbody>
+              <input type="hidden" name="intOpcion" value="1">
+            <?php
+              $intContadorPasos = 1;
+              foreach ($listPasosVideos as $key) {
+                echo '<tr class="info">
+                        <td>'.$intContadorPasos.'</td>
+                        <td><input type="text" class="form-control" name="decId_'.$intContadorPasos.'" placeholder="Url youtube" value="'.$key->strURL.'"></td>
+                        <td><input type="text" class="form-control" name="strAyuda_'.$intContadorPasos.'" placeholder="Link pagina ayuda" value="'.$key->strAyudaLink.'"></td>
+                      </tr>';
+                $intContadorPasos += 1;
+              }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    <button type="submit" class="btn btn-primary btn-sm btn-block">Guardar</button>
+    </form>
   </div>
-<button type="submit" class="btn btn-primary btn-sm btn-block">Guardar</button>
-</form>
+</div>
 
 <?php  } ?>
 
